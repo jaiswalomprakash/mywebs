35170bf6d85a6b349b29966f582fea76484b859e
omprakash <jaiswal.omprakashjaiswal@gmail.com>
2016-02-22 19:43:46 +0530

    chnaged

---
+++ src/com/webservices/custom/modal/DailyServiceRequest.java
@@ -0,0 +1,66 @@
+package com.webservices.custom.modal;
+
+import java.util.Date;
+
+public class DailyServiceRequest {
+	
+	private Long id;
+    private String itemDesc;	
+	private Integer type;
+	private Integer userId;	
+	private String active;
+	private Float price;
+	private Long serviceId;	
+	private String token;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getItemDesc() {
+		return itemDesc;
+	}
+	public void setItemDesc(String itemDesc) {
+		this.itemDesc = itemDesc;
+	}
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}	
+	public String getActive() {
+		return active;
+	}
+	public void setActive(String active) {
+		this.active = active;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	public Long getServiceId() {
+		return serviceId;
+	}
+	public void setServiceId(Long serviceId) {
+		this.serviceId = serviceId;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+
+}
